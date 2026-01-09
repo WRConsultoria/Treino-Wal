d67de29445a00fd4a2ba1adfc0a5a91ef22f2dc5
WRConsultoria <walmir.rezende@gmail.com>
2026-01-09 14:32:52 -0300

    PWA: use relative manifest, icons and register SW with relative path for Pages

--- script.js
+++ script.js
@@ -255,7 +255,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // --- PWA Service Worker Registration ---
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("./sw.js")
       .then((registration) => {
         console.log("Service Worker registered with scope:", registration.scope);
       })
